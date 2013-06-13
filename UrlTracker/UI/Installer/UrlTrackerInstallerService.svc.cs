c0f8f5cbde204ea167d1e629cf136f1409f426c6
Stefan Kip <mail@kipusoep.nl>
2013-06-13 14:56:45 +0200

    Added dashboard installer

--- UrlTracker/UI/Installer/UrlTrackerInstallerService.svc.cs
+++ UrlTracker/UI/Installer/UrlTrackerInstallerService.svc.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading;
 using System.Web;
 using System.Web.Script.Serialization;
+using System.Xml.Linq;
 
 namespace InfoCaster.Umbraco.UrlTracker.UI.Installer
 {
@@ -27,7 +28,59 @@ namespace InfoCaster.Umbraco.UrlTracker.UI.Installer
 		{
 			try
 			{
+				Wait();
 				UrlTrackerRepository.CreateUrlTrackerTable();
+			}
+			catch (Exception ex)
+			{
+				return HandleException(ex);
+			}
+			return string.Empty;
+		}
+
+		[OperationContract]
+		[WebGet]
+		public string InstallDashboard()
+		{
+			try
+			{
+				Wait();
+				string dashboardConfig;
+				string dashboardConfigPath = HttpContext.Current.Server.MapPath("~/config/dashboard.config");
+				using (StreamReader streamReader = File.OpenText(dashboardConfigPath))
+					dashboardConfig = streamReader.ReadToEnd();
+				XDocument dashboardDoc = XDocument.Parse(dashboardConfig, LoadOptions.PreserveWhitespace);
+				XElement startupDashboardSectionElement = dashboardDoc.Element("dashBoard").Elements("section").SingleOrDefault(x => x.Attribute("alias").Value == "StartupDashboardSection");
+				if (startupDashboardSectionElement == null)
+					throw new Exception("Unable to add dashboard: StartupDashboardSection not found in ~/config/dashboard.config");
+
+				List<XElement> tabs = startupDashboardSectionElement.Elements("tab").ToList();
+				if(!tabs.Any())
+					throw new Exception("Unable to add dashboard: No existing tabs found within the StartupDashboardSection");
+
+				List<XElement> urlTrackerTabs = tabs.Where(x => x.Attribute("caption").Value == "Url Tracker").ToList();
+				if (urlTrackerTabs.Any())
+				{
+					foreach(XElement tab in urlTrackerTabs)
+					{
+						List<XElement> urlTrackerTabControls = tab.Elements("control").ToList();
+						if (urlTrackerTabControls.Any(x => x.Value == "/Umbraco/UrlTracker/InfoCaster.Umbraco.UrlTracker.UI.UrlTrackerManagerWrapper.ascx"))
+							throw new Exception("Dashboard is already installed.");
+					}
+				}
+
+				XElement lastTab = tabs.Last();
+				XElement urlTrackerTab = new XElement("tab");
+				urlTrackerTab.Add(new XAttribute("caption", "Url Tracker"));
+				XElement urlTrackerControl = new XElement("control");
+				urlTrackerControl.Add(new XAttribute("addPanel", true));
+				urlTrackerControl.SetValue("/Umbraco/UrlTracker/InfoCaster.Umbraco.UrlTracker.UI.UrlTrackerManagerWrapper.ascx");
+				urlTrackerTab.Add(urlTrackerControl);
+				urlTrackerControl.AddBeforeSelf(string.Concat(Environment.NewLine, "      "));
+				urlTrackerControl.AddAfterSelf(string.Concat(Environment.NewLine, "    "));
+				lastTab.AddAfterSelf(urlTrackerTab);
+				lastTab.AddAfterSelf(string.Concat(Environment.NewLine, "    "));
+				dashboardDoc.Save(dashboardConfigPath, SaveOptions.None);
 			}
 			catch (Exception ex)
 			{
@@ -42,6 +95,7 @@ namespace InfoCaster.Umbraco.UrlTracker.UI.Installer
 		{
 			try
 			{
+				Wait();
 				Uri currentUri = HttpContext.Current.Request.Url;
 				WebRequest request = WebRequest.Create(string.Format("{0}://{1}?{2}=1", currentUri.Scheme, currentUri.Host, UrlTrackerSettings.HttpModuleCheck));
 				using (WebResponse response = request.GetResponse())
@@ -64,6 +118,7 @@ namespace InfoCaster.Umbraco.UrlTracker.UI.Installer
 		{
 			try
 			{
+				Wait();
 				return UrlTrackerRepository.GetUrlTrackeOldTableExists().ToString();
 			}
 			catch (Exception ex)
@@ -78,6 +133,7 @@ namespace InfoCaster.Umbraco.UrlTracker.UI.Installer
 		{
 			try
 			{
+				Wait();
 				return UrlTrackerRepository.MigrateData().ToString();
 			}
 			catch (Exception ex)
@@ -92,6 +148,11 @@ namespace InfoCaster.Umbraco.UrlTracker.UI.Installer
 			if(ex.InnerException != null)
 				return string.Format("error: {0} ({1})", ex.Message, ex.InnerException.Message);
 			return string.Concat("error: ", ex.Message);
+		}
+
+		void Wait()
+		{
+			Thread.Sleep(1000);
 		}
 	}
 }
--- UrlTracker/UI/res/js/installer.js
+++ UrlTracker/UI/res/js/installer.js
@@ -14,56 +14,66 @@ $(function () {
 				data = data.d;
 				showInfoErrorOrSuccess("Installing the database table failed:", "Installing the database table skipped:", data, "Table already exists.", "Installed the database table.");
 
-				// Step 2; check HttpModule
-				$urlTrackerLoader.append($("<p />").text("Checking the Http Module."));
+				// Step 2; install dashboard
+				$urlTrackerLoader.append($("<p />").text("Installing the dashboard."));
 				$.getJSON(
-					serviceUrl + "/checkhttpmodule",
+					serviceUrl + "/installdashboard",
 					function (data, textStatus, jqXHR) {
 						data = data.d;
-						showErrorOrSuccess("The Http Module isn't installed:", data, "The Http Module is functional.");
+						showInfoErrorOrSuccess("Installing the dashboard failed:", "Installing the dashboard skipped:", data, "Dashboard is already installed.", "Installed the dashboard.");
 
-						// Step 3; check if old version exists
-						$urlTrackerLoader.append($("<p />").text("Checking if a previous version exists."));
+						// Step 3; check HttpModule
+						$urlTrackerLoader.append($("<p />").text("Checking the Http Module."));
 						$.getJSON(
-							serviceUrl + "/hasoldversioninstalled",
+							serviceUrl + "/checkhttpmodule",
 							function (data, textStatus, jqXHR) {
 								data = data.d;
-								showErrorOrSuccess("Checking for previous version failed:", data, "Previous version checked succesfully.");
-								if (data.toLowerCase() == "true") {
+								showErrorOrSuccess("The Http Module isn't installed:", data, "The Http Module is functional.");
 
-									// Step 4; migrate data of old version
-									$urlTrackerLoader.append($("<p />").text("Previous version found! Would you like to migrate the existing data?"));
-									var yesButton = $("<a />").attr("href", "#").text("Yes please").addClass("btn").addClass("btn-primary");
-									yesButton.click(function () {
-										yesButton.remove();
-										noButton.remove();
-										showLoadingIcon();
-										$urlTrackerLoader.append($("<p />").addClass("info").text("Yes please"));
-										$urlTrackerLoader.append($("<p />").text("Migrating existing data."));
-										$.getJSON(
-											serviceUrl + "/migratedata",
-											function (data, textStatus, jqXHR) {
-												data = data.d;
-												showErrorOrSuccess("Migrating data failed:", data, "Migrating succeeded, entries migrated: " + data);
+								// Step 4; check if old version exists
+								$urlTrackerLoader.append($("<p />").text("Checking if a previous version exists."));
+								$.getJSON(
+									serviceUrl + "/hasoldversioninstalled",
+									function (data, textStatus, jqXHR) {
+										data = data.d;
+										showErrorOrSuccess("Checking for previous version failed:", data, "Previous version checked succesfully.");
+										if (data.toLowerCase() == "true") {
+
+											// Step 5; migrate data of old version
+											$urlTrackerLoader.append($("<p />").text("Previous version found! Would you like to migrate the existing data?"));
+											var yesButton = $("<a />").attr("href", "#").text("Yes please").addClass("btn").addClass("btn-primary");
+											yesButton.click(function () {
+												yesButton.remove();
+												noButton.remove();
+												showLoadingIcon();
+												$urlTrackerLoader.append($("<p />").addClass("info").text("Yes please"));
+												$urlTrackerLoader.append($("<p />").text("Migrating existing data."));
+												$.getJSON(
+													serviceUrl + "/migratedata",
+													function (data, textStatus, jqXHR) {
+														data = data.d;
+														showErrorOrSuccess("Migrating data failed:", data, "Migrating succeeded, entries migrated: " + data);
+														showFinishedMessage();
+													}
+												);
+											});
+											$urlTrackerLoader.append(yesButton);
+											var noButton = $("<a />").attr("href", "#").text("No thanks").addClass("btn");
+											noButton.click(function () {
+												yesButton.remove();
+												noButton.remove();
+												$urlTrackerLoader.append($("<p />").addClass("info").text("No thanks"));
 												showFinishedMessage();
-											}
-										);
-									});
-									$urlTrackerLoader.append(yesButton);
-									var noButton = $("<a />").attr("href", "#").text("No thanks").addClass("btn");
-									noButton.click(function () {
-										yesButton.remove();
-										noButton.remove();
-										$urlTrackerLoader.append($("<p />").addClass("info").text("No thanks"));
-										showFinishedMessage();
-									});
-									$urlTrackerLoader.append(noButton);
-									hideLoadingIcon();
-								}
-								else {
-									$urlTrackerLoader.append($("<p />").addClass("info").text("Previous version not found."));
-									showFinishedMessage();
-								}
+											});
+											$urlTrackerLoader.append(noButton);
+											hideLoadingIcon();
+										}
+										else {
+											$urlTrackerLoader.append($("<p />").addClass("info").text("Previous version not found."));
+											showFinishedMessage();
+										}
+									}
+								);
 							}
 						);
 					}
