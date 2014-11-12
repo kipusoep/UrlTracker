85302e4755f75efbe016e2186aeedf053b39355c
SandroMastronardi <sandro.mastronardi@mastrosoft.com>
2014-11-12 10:16:23 +0100

    added compatability for SqlCompact

---
+++ SQL/SqlServerCompact/create-table-1.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [icUrlTracker] (
+  [Id] int NOT NULL  IDENTITY (1,1)
+, [OldUrl] nvarchar(400) NULL
+, [OldUrlQueryString] nvarchar(400) NULL
+, [OldRegex] nvarchar(400) NULL
+, [RedirectRootNodeId] int NULL
+, [RedirectNodeId] int NULL
+, [RedirectUrl] nvarchar(400) NULL
+, [RedirectHttpCode] int NOT NULL DEFAULT ((301))
+, [RedirectPassThroughQueryString] bit NOT NULL DEFAULT ((1))
+, [Notes] nvarchar(400) NULL
+, [Is404] bit NOT NULL DEFAULT ((0))
+, [Referrer] nvarchar(400) NULL
+, [Inserted] datetime NOT NULL DEFAULT (getdate())
+)
