9b83f3d7cd341d0cc7d21e195e33323b83a54a83
kipusoep <mail@kipusoep.nl>
2014-12-11 16:39:29 +0100

    Updated version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Web.UI;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.2.*")]
+[assembly: AssemblyVersion("3.3.*")]
 
 // SQL
 [assembly: WebResource("InfoCaster.Umbraco.UrlTracker.SQL.MicrosoftSqlServer.create-table-1.sql", "text/plain")]
